15cad4ce7408babcaa51ee4f9f92d05465bb7758
annashutova <annashut@inbox.ru>
2023-10-23 23:56:44 +0300

    added: user and role models

---
+++ app/models.py
@@ -0,0 +1,40 @@
+from app.extensions import db
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import func
+from datetime import datetime
+import uuid
+import config
+
+
+class UUIDMixin:
+    """Inherit to add uuid primary key column to a model."""
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+
+class TimestampMixin:
+    """Inherit to add created_at and modified_at columns to a model."""
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    modified_at = db.Column(db.DateTime, nullable=False, onupdate=func.timezone('UTC', func.current_timestamp()))
+
+
+class Role(UUIDMixin, TimestampMixin, db.Model):
+    __tablename__ = 'role'
+
+    title = db.Column(db.String(config.DEFAULT_STRING_VALUE), unique=True)
+    users = db.relationship('User', backref='user', lazy=True, cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<Role {self.title}>'
+
+
+class User(UUIDMixin, TimestampMixin, db.Model):
+    __tablename__ = 'user'
+
+    first_name = db.Column(db.String(config.DEFAULT_STRING_VALUE))
+    last_name = db.Column(db.String(config.DEFAULT_STRING_VALUE))
+    email = db.Column(db.String(config.DEFAULT_STRING_VALUE), nullable=False, unique=True)
+    password = db.Column(db.String(config.PASSWORD_LENGTH), nullable=False)
+    role = db.Column(UUID(as_uuid=True), db.ForeignKey('role.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
