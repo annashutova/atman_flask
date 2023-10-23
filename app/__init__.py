3f6955b314a064285681ee1cb89d87733a04ee6a
annashutova <annashut@inbox.ru>
2023-10-23 23:54:26 +0300

    added: app fabric function

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+"""Flask application."""
+from flask import Flask
+from config import Config, DevConfig
+from app.extensions import db, migrate
+
+
+def create_app(config_class: Config = DevConfig):
+    """Factory function for creating flask app."""
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Initialize Flask extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register blueprints
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    return app
