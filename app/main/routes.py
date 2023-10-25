163446f4be84c4bb53631096ced4b101ab959839
annashutova <annashut@inbox.ru>
2023-10-25 15:38:07 +0300

    added: main routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,23 @@
 """Routes of main blueprint."""
+from loguru import logger
 from app.main import bp
-from flask import request
-from flask import redirect, url_for, make_response, render_template
+from flask import request, redirect, url_for, make_response, render_template, current_app
+from app.extensions import mail
+from flask_mail import Message
 
 
+@bp.route('/')
+def index():
+    return render_template('index.html')
+
+@bp.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        site_email = current_app.config.get('MAIL_USERNAME')
+        msg = Message('Обратная связь', recipients=[site_email])
+        msg.body = f'Вы получили сообщение от пользователя: email: {email}, телефон: {phone}'
+        mail.send(msg)
+        logger.info('Email successfuly sent')
+    return redirect(url_for('main.index'))
