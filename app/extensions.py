d26d6dfb9f08bc952911022ae970052ac2105143
annashutova <annashut@inbox.ru>
2023-10-25 07:39:54 +0300

    added: flask login extension

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,8 @@
 """Extensions for the app."""
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
