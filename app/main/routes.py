92d800cecd32ed11813d8bf60978fe228b07e6f7
annashutova <annashut@inbox.ru>
2023-10-27 22:47:29 +0300

    added: routes for static pages

--- app/main/routes.py
+++ app/main/routes.py
@@ -19,5 +19,28 @@ def contact():
         msg = Message('Обратная связь', recipients=[site_email])
         msg.body = f'Вы получили сообщение от пользователя: email: {email}, телефон: {phone}'
         mail.send(msg)
-        logger.info('Email successfuly sent')
     return redirect(url_for('main.index'))
+
+@bp.route('/agreement')
+def agreement():
+    return render_template('documents/agreement.html')
+
+@bp.route('/privacy-policy')
+def privacy_policy():
+    return render_template('documents/privacy-policy.html')
+
+@bp.route('/requisites')
+def requisites():
+    return render_template('documents/requisites.html')
+
+@bp.route('/guarantees-and-ereturns')
+def guarantees_ereturns():
+    return render_template('information/guarantees-and-ereturns.html')
+
+@bp.route('/payment-delivery')
+def payment_delivery():
+    return render_template('information/payment-delivery.html')
+
+@bp.route('/help')
+def help():
+    return render_template('information/help.html')
