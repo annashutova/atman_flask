28d293a1e35e652a548fd3c15259a363b709ad26
annashutova <annashut@inbox.ru>
2023-11-23 01:36:05 +0300

    added: jsfor product html page

--- app/static/javascript/product_quantity.js
+++ app/static/javascript/product_quantity.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const decreaseBtns = document.querySelectorAll('.decreaseBtn')
-    const increaseBtns = document.querySelectorAll('.increaseBtn')
-    const quantityInputs = document.querySelectorAll('.quantityInput')
+    let increaseBtn = document.getElementById('increaseBtn')
+    let decreaseBtn = document.getElementById('decreaseBtn')
+    let quantityInput = document.getElementById('quantityInput')
+    let buyButton = document.getElementById('buyBtn')
+
+    const productId = buyButton.getAttribute('product-id')
 
     const changeButtonState = (button, buttonClicked) => {
         if (buttonClicked) {
@@ -23,95 +26,81 @@ document.addEventListener('DOMContentLoaded', function() {
         localStorage.setItem(productId, JSON.stringify(actionData))
     }
 
-    const setDataFromLocalStorage = (productId, index) => {
+    const setDataFromLocalStorage = (productId) => {
         const storedData = localStorage.getItem(productId)
         if (storedData) {
             const parsedData = JSON.parse(storedData)
-            quantityInputs[index].value = parsedData.quantity
-            increaseBtns[index].disabled = parsedData.btnDisabled
-
-            const buyButton = document.querySelector(`button[button-id="${index}"]`)
+            quantityInput.value = parsedData.quantity
+            increaseBtn.disabled = parsedData.btnDisabled
             changeButtonState(buyButton, true)
         }
     }
 
-    quantityInputs.forEach((input, index) => {
-        const productId = input.getAttribute('product-id')
-        setDataFromLocalStorage(productId, index)
+    const checkQuantity = () => {
+        let quantity = parseInt(quantityInput.value)
+        const stock = parseInt(quantityInput.getAttribute('max'))
+        if (quantity === stock) {
+            increaseBtn.disabled = true
+        }
+    }
+
+    checkQuantity()
+    setDataFromLocalStorage(productId)
+
+    buyButton.addEventListener('click', function () {
+        const form = buyButton.closest('.addToCartForm')
+        const formData = new FormData(form)
+
+        let btnDisabled = increaseBtn.disabled
+
+        fetch(form.action, {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'success') {
+                let quantity = formData.get('quantity')
+                saveProdState(productId, quantity, btnDisabled)
+                changeButtonState(buyButton, true)
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error)
+        })
     })
 
-    document.addEventListener('click', function (event) {
-        if (event.target && event.target.matches('.btn.btn-danger')) {
-            let buyButton = event.target
-            const form = buyButton.closest('.addToCartForm')
-
-            const formData = new FormData(form)
-            const productId = buyButton.getAttribute('product-id')
-            const buttonId = buyButton.getAttribute('button-id')
-            let btnDisabled = increaseBtns[parseInt(buttonId)].disabled
-
-            fetch(form.action, {
-                method: 'POST',
-                body: formData
-            })
-            .then(response => response.json())
-            .then(data => {
-                if (data.status === 'success') {
-                    let quantity = formData.get('quantity')
-                    saveProdState(productId, quantity, btnDisabled)
-                    changeButtonState(buyButton, true)
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error)
-            })
+    decreaseBtn.addEventListener('click', function() {
+        let quantity = parseInt(quantityInput.value)
+        changeButtonState(buyButton, false)
+        if (quantity > 1) {
+            quantity--
+            quantityInput.value = quantity
+            increaseBtn.disabled = false
         }
     })
 
-    decreaseBtns.forEach((decreaseBtn, index) => {
-        decreaseBtn.addEventListener('click', function() {
-            let quantity = parseInt(quantityInputs[index].value)
-            let buyButton = document.querySelector(`button[button-id="${index}"]`)
-            let increaseBtn = increaseBtns[index]
-            changeButtonState(buyButton, false)
-            if (quantity > 1) {
-                quantity--
-                quantityInputs[index].value = quantity
-                increaseBtn.disabled = false
-            }
-        })
+    increaseBtn.addEventListener('click', function() {
+        const stock = parseInt(quantityInput.getAttribute('max'))
+        let quantity = parseInt(quantityInput.value)
+        changeButtonState(buyButton, false)
+        quantity++
+        quantityInput.value = quantity
+        if (quantity === stock) {
+            increaseBtn.disabled = true
+        }
     })
 
-    increaseBtns.forEach((increaseBtn, index) => {
-        increaseBtn.addEventListener('click', function() {
-            let input = quantityInputs[index]
-            const stock = parseInt(input.getAttribute('max'))
-            let quantity = parseInt(input.value)
-            let buyButton = document.querySelector(`button[button-id="${index}"]`)
-            changeButtonState(buyButton, false)
-            quantity++
-            input.value = quantity
-            if (quantity === stock) {
-                increaseBtn.disabled = true
-            }
-        })
-    })
-
-    quantityInputs.forEach((input) => {
-        input.addEventListener('input', function() {
-            let quantity = parseInt(input.value)
-            const stock = parseInt(input.getAttribute('max'))
-            const buttonId = input.getAttribute('button-id')
-            let increaseBtn = increaseBtns[parseInt(buttonId)]
-            let buyButton = document.querySelector(`button[button-id="${buttonId}"]`)
-            changeButtonState(buyButton, false)
-            if (isNaN(quantity) || quantity < 1) {
-                input.value = 1
-                increaseBtn.disabled = false
-            } else if (quantity >= stock) {
-                input.value = stock
-                increaseBtn.disabled = true
-            }
-        })
+    quantityInput.addEventListener('input', function() {
+        let quantity = parseInt(quantityInput.value)
+        const stock = parseInt(quantityInput.getAttribute('max'))
+        changeButtonState(buyButton, false)
+        if (isNaN(quantity) || quantity < 1) {
+            quantityInput.value = 1
+            increaseBtn.disabled = false
+        } else if (quantity >= stock) {
+            quantityInput.value = stock
+            increaseBtn.disabled = true
+        }
     })
 })
