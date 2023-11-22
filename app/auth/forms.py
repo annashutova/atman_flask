d22f5ce7a05d084631d5848984bc745efc826fd2
annashutova <annashut@inbox.ru>
2023-11-23 01:31:34 +0300

    added: 2 methods for login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,6 @@
 from wtforms import StringField, PasswordField, validators, ValidationError
 from flask_wtf import FlaskForm
+from werkzeug.security import check_password_hash
 from app.models import User
 
 
@@ -29,3 +30,11 @@ class LoginForm(FlaskForm):
         [validators.Email(message='Невалидная почта.'), validators.DataRequired(message='Это обязательное поле.')]
     )
     password = PasswordField('Пароль: ', [validators.DataRequired(message='Это обязательное поле.')])
+
+    def get_user(self) -> User | None:
+        return User.query.filter_by(email=self.email.data).first()
+    
+    def validate_user(self):
+        user = self.get_user()
+        if not (user and check_password_hash(user.password, self.password.data)):
+            raise ValidationError('Неверный логин или пароль.')
