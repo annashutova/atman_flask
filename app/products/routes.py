f7a256839860da9ecf10376bcc055e4536b5fa36
annashutova <annashut@inbox.ru>
2023-11-25 03:51:33 +0300

    added: routes for retreiving user orders

--- app/products/routes.py
+++ app/products/routes.py
@@ -1,8 +1,9 @@
 """Routes of products blueprint."""
 from loguru import logger
 from flask import render_template, url_for, redirect
+from flask_login import login_required, current_user
 from app.products import bp
-from app.models import Category, Product
+from app.models import Category, Product, OrderDetail
 from uuid import UUID
 
 
@@ -36,3 +37,16 @@ def get_product(product_id):
         return redirect(url_for('product.load_catalog'))
     breadcrumbs = get_breadcrumbs(product.category_id)
     return render_template('products/product.html', breadcrumbs=breadcrumbs, product=product)
+
+@bp.route("/orders")
+@login_required
+def get_user_orders():
+    actual_orders = OrderDetail.query.filter(OrderDetail.user_id == current_user.id, OrderDetail.status == 'в процессе').all()
+    past_orders = OrderDetail.query.filter(OrderDetail.user_id == current_user.id, OrderDetail.status == 'получен').all()
+    return render_template('orders/user_orders.html', actual_orders=actual_orders, past_orders=past_orders)
+
+@bp.route("order/<uuid:order_id>")
+@login_required
+def get_order(order_id):
+    order = OrderDetail.query.get(order_id)
+    return render_template('orders/order_item.html', items=order.product_association, order=order)
